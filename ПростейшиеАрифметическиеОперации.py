7694b2f0979c3902b0c11887f1dbeedc7cdab8a5
Taras <taras18taras@gmail.com>
2018-02-14 09:51:48 +0200

    Decode profram 3 variants

---
+++ ПростейшиеАрифметическиеОперации.py
@@ -0,0 +1,14 @@
+def arithmetic(arg1, arg2, op):
+     if op == '+':
+         return arg1 + arg2
+     elif op == '-':
+         return arg1 - arg2
+     elif op == '*':
+         return arg1 * arg2
+     elif op == '/':
+         return arg1 / arg2
+     else:
+        return "Неизвестная операция"
+
+#print(arithmetic(1,2,"+"))
+print(2)
