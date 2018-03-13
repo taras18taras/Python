819ec77c544c1337c9c065df39783f2aabd572c5
Taras <taras18taras@gmail.com>
2018-03-13 12:02:00 +0200

    games

--- TEST.py
+++ TEST.py
@@ -1,5 +1,3 @@
-f = 0.1*3==0.3
-
-
-print(0.1*3)
-print(f)
+aristotle_born = date(year=-322, month=10, day=1)
+nietzsche_born = date(year=1884, month=9, day=15)
+print((nietzsche_born - aristotle_born).day)
